260a54658efce836ef18cb47f8d38c63125a3754
jazir788 <jazirsx5@gmail.com>
2023-10-27 13:21:06 +0100

    removed print functions

--- send_email.py
+++ send_email.py
@@ -8,11 +8,6 @@ def sendEmail(message):
     username = "jazirtest@gmail.com"
     password = os.getenv("PASSWORD")
     receiver = "jazirtest@gmail.com"
-<<<<<<< HEAD
-=======
-    print(username)
-    print(password)
->>>>>>> origin/master
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context = context) as server:
