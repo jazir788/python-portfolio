83d9f9249c3f4bae4b87807345baec80a264f727
jazir788 <jazirsx5@gmail.com>
2023-10-27 13:19:28 +0100

    Added send email functionality by creating a send email script

---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+import ssl
+import os
+
+def sendEmail(message):
+    host = "smtp.gmail.com"
+    port= 465
+    username = "jazirtest@gmail.com"
+    password = os.getenv("PASSWORD")
+    receiver = "jazirtest@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context = context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
