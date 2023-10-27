8194433978b634eb266ea14f0a1b5cd2439c3a0f
jazir788 <jazirsx5@gmail.com>
2023-10-27 11:44:00 +0100

    Added 2 additional columns and project title, description and images

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, emptycol1, col2 = st.columns([1.5,0.5,1.5])
 
 with col1:
-    st.image("images/me2.jpg")
+    st.image("images/me_2.jpg")
 with col2:
     st.title("Jazir Shine")
     content = """ 
@@ -19,3 +20,28 @@ with col2:
     and make a positive impact on future projects.
     """
     st.info(content)
+
+content2 = """Below you can find some of the apps I have built in Python. Feel free to contact me! """
+st.write(content2)
+
+col3, emptycol2, col4 = st.columns([1.5,0.5,1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with (col3):
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write("[Source Code](Link)")
+
+
+
+
+
